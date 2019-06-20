4a3ca97d25e460e0f78874b3ba995d43636544ed
Eros Reale <eros.francesco.francesco@gmail.com>
2019-06-20 14:38:21 +0200

    another fix

--- src/app/services/utils/resource.service.ts
+++ src/app/services/utils/resource.service.ts
@@ -8,7 +8,7 @@ import { Resource } from "./resource.model"
 export class ResourceService {
 
   category: string
-  items: [Resource]
+  items: [Resource] = [null]
   
   
   constructor(category: string) { 
